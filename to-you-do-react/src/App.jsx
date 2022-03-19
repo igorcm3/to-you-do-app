e8c16c45ffa3771e8a00c02f8a415ca714d3d686
Igor Corona de Matos <32557238+igorcm3@users.noreply.github.com>
2022-03-18 21:42:51 -0300

    feat: button component - parcial

--- to-you-do-react/src/App.jsx
+++ to-you-do-react/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <h1>Olá mundo!</h1>
+      <Button text="teste" />
     </div>
   )
 }
---
+++ to-you-do-react/src/components/Button/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Button(props){
+    return <StyledButton>{props.text}</StyledButton>;
+}
+
+export default Button;
