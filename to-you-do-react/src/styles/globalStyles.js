3cb51c577a1ad61d161e477efdfc4d4e8eb52134
Igor Corona de Matos <32557238+igorcm3@users.noreply.github.com>
2022-03-18 21:13:49 -0300

    config: folder structure and global styles

---
+++ to-you-do-react/src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: 'Roboto', sans-serif;
+
+  }
+`;
+ 
+export default GlobalStyle;
