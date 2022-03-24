c7009fdbd9a7f27fcf319373456790da20f46c90
Igor Corona de Matos <32557238+igorcm3@users.noreply.github.com>
2022-03-23 23:25:05 -0300

    feat: footer singup

--- to-you-do-react/src/pages/Login.js
+++ to-you-do-react/src/pages/Login.js
@@ -5,6 +5,7 @@ import Input from '../components/Input';
 import Button from '../components/Button';
 import { EntryPage, PageHeader } from './style';
 import logo from '../assets/img/logo-md.png'
+import { Link } from 'react-router-dom'
 
 
 function Login(){
@@ -17,16 +18,18 @@ function Login(){
                 <h2>Log in</h2>
                 <form onSubmit={(e) => e.preventDefault()}>
                     <InputGroup>
-                        <label htmlFor='login-email'>Endereço de email</label>
+                        <label htmlFor='login-email'>Email:</label>
                         <Input type="text" placeholder="nome@email.com" id="login-email" />
                     </InputGroup>
                     <InputGroup>
-                        <label htmlFor='login-senha'>Senha</label>
+                        <label htmlFor='login-senha'>Senha:</label>
                         <Input type="password" placeholder="senha" id="login-senha" />
                     </InputGroup>      
                     <Button type="submit" full>Entrar</Button>              
-
                 </form>
+                <span>
+                    Não tem uma conta? <Link to="/singup">Cadastre-se</Link>
+                </span>
             </EntryCard>
         </EntryPage>
 
